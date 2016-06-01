1b4854a3356089a36634f21970636d5acc7a09c5
junneyang <597092663@qq.com>
2016-06-01 17:30:41 +0800

    msg: mysql support exe sql scripts on boot

--- mysql/5.6/applications/build-depends/mysql-conf/mysql_init.sh
+++ mysql/5.6/applications/build-depends/mysql-conf/mysql_init.sh
@@ -12,8 +12,14 @@ if [ -z "${files}" ]; then
         && mysql -e "grant all privileges on *.* to 'root'@'localhost' identified by '${MYSQL_ROOT_PASSWORD}';" \
         && mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE ${MYSQL_DATABASE};" \
         && mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "grant all privileges on *.* to '${MYSQL_USER}'@'%' identified by '${MYSQL_PASSWORD}';" \
-        && mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "grant all privileges on *.* to '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWORD}';" \
-        && killall mysqld
+        && mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "grant all privileges on *.* to '${MYSQL_USER}'@'localhost' identified by '${MYSQL_PASSWORD}';" 
+    if [ "${SVN_REPO_URL:-NOTDEF}" != "NOTDEF" ]; then
+        echo "need to execute sql scripts"
+        cd ${APP_PATH}/deploy/scripts/ && ./execute_sql.sh
+    else
+        echo "no need to execute sql scripts, skip"
+    fi
+    killall mysqld
     echo 'Database initialized'
 else
     echo "${datafile} is not empty, database is initialized, skip"
