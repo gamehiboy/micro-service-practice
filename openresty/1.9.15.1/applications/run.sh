fff185c20301447ee7e2978e385313964407860d
junneyang <597092663@qq.com>
2016-06-17 09:36:41 +0800

    msg: base demo 4 openresty

--- openresty/1.9.15.1/applications/run.sh
+++ openresty/1.9.15.1/applications/run.sh
@@ -2,6 +2,11 @@
 set -eux
 env
 
+# config OpenResty
+cp -rf ${APP_PATH}/nginx-conf.d ${APP_PATH}/openresty/nginx/conf/
+mv ${APP_PATH}/openresty/nginx/conf/nginx.conf ${APP_PATH}/openresty/nginx/conf/nginx.conf.bak
+cp -rf ${APP_PATH}/nginx-conf.d/nginx.conf ${APP_PATH}/openresty/nginx/conf/
+
 # start supervisord nodaemon
 python ${APP_PATH}/build-utils/configueUpdate/templateInvoke.py ${APP_PATH}/supervisor.conf.d/nginx.conf.template ${APP_PATH}/supervisor.conf.d/nginx.conf
 cp ${APP_PATH}/supervisor.conf.d/*.conf /etc/supervisor/conf.d/
